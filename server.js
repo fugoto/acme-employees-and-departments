790ff999e9a97df02f313f2a370e89582f11bed3
Fu Goto <fu.goto@gmail.com>
2020-08-25 17:47:13 -0400

    final

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded( { extended: false }))
 
 app.use('/dist',express.static(path.join(__dirname, 'dist')))
-
 app.use(express.static(path.join(__dirname,'./public')))
 
 app.get('/',(req,res,next) => {
